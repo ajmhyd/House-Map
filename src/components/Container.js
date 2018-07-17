b4926a4cb7bd68a9527eb3e5c789229cd3ea5db9
ajmhyd <ajmhyd@mail.missouri.edu>
2018-07-16 20:10:55 -0500

    Add markers

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,29 @@ import { GoogleApiWrapper, Map, InfoWindow, Marker } from 'google-maps-react'
 
 
 export class Container extends Component {
+
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+    };
+
+    onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+          this.setState({
+            showingInfoWindow: false,
+            activeMarker: null
+          })
+        }
+    };
+
     render () {
         const style = {
             width: '100vw',
@@ -12,10 +35,57 @@ export class Container extends Component {
             return <div>Loading...</div>
         }
         return (
-            <div style={style}>
-            <Map google={this.props.google} >
+            <Map
+                google={this.props.google}
+                style={style}
+                initialCenter={{
+                    lat: 41.887832,
+                    lng: -87.623177
+                }}
+                zoom={12}
+                onClick={this.onMapClicked}>
+                <Marker
+                    name={'Primary'}
+                    position={{lat: 41.903670, lng: -87.628941}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'The Mid'}
+                    position={{lat: 41.887083, lng: -87.647720}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'Spybar'}
+                    position={{lat: 41.893647, lng: -87.635909}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'Sound Bar'}
+                    position={{lat: 41.893414, lng: -87.635315}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'Prysm'}
+                    position={{lat: 41.909330, lng: -87.652520}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'Studio Paris'}
+                    position={{lat: 41.889786, lng: -87.630296}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'Berlin'}
+                    position={{lat: 41.940024, lng: -87.653873}}
+                    onClick={this.onMarkerClick} />
+                <Marker
+                    name={'Smartbar'}
+                    position={{lat: 41.949772, lng: -87.658822}}
+                    onClick={this.onMarkerClick} />
+                <InfoWindow
+                    onOpen={this.windowHasOpened}
+                    onClose={this.windowHasClosed}
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
             </Map>
-            </div>
         )
     }
 }
