66c61a3727785aef587e42ddfe147bfd76871dbd
ajmhyd <ajmhyd@mail.missouri.edu>
2018-07-17 13:17:52 -0500

    Add club data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Container from './components/Container';
 import { Navbar } from 'react-bootstrap'
+import Clubs from './data/Clubs.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clubs : []
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      clubs: Clubs
+    })
+
+  }
   render() {
+    console.log(this.state.clubs)
     return (
       <div className="App">
         <Navbar>
